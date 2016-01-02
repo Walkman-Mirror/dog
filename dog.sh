caeac68a9e208e98d9a9ef07766f13bda29a1f3b
Tristan B. Kildaire (CarterPi) <deavmi@gmail.com>
2016-01-02 11:26:55 +0200

    Added commnad-line argumnet parsing.

--- dog.sh
+++ dog.sh
@@ -1,3 +1,27 @@
+#dog
+
+
+VERSION=0.0.1
+
+function showHelp
+{
+
+	about="dog v$VERSION\n
+Written by: Tristan B. Kildaire (Deavmi) - (deavmi@disroot.org)
+
+"
+
+	whiptail --msgbox "$about" 20 100 --fullbuttons
+
+}
+
+function showVersion
+{
+
+	echo $VERSION
+
+}
+
 function showFile
 {
 
@@ -8,3 +32,33 @@ function showFile
 	whiptail --msgbox "$content" 20 100 --title "$filename"
 
 }
+
+function init
+{
+
+	if [ $# = 0 ]
+	then
+		echo "Use --help for a list of commands."
+	elif [ $# = 1 ]
+	then
+		if [ "$1" = "--help" ]
+		then
+			showHelp
+		elif [ "$1" = "--version" ]
+		then
+			showVersion
+		else
+			if [ -f "$1" ]
+			then
+				showFile "$1"
+			else
+				echo "File '$1' not found!"
+			fi
+		fi
+	else
+		echo "Unknown commands $@! Use --help for a list of commands."
+	fi
+
+}
+
+init $@
